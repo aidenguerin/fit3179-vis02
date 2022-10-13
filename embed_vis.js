f07c66845cadea8ae71f77ad64dc949cdebdda48
Aiden Guerin <aiden.guerin@outlook.com>
2022-10-13 17:25:42 +1100

    moved dir

--- embed_vis.js
+++ embed_vis.js
@@ -1,12 +1,12 @@
 console.log("vegalite_vis.js loaded");
 
-var vl_map_na = "../vega-lite/map_na.vl.json"
+var vl_map_na = "vega-lite/map_na.vl.json"
 vegaEmbed("#map_na", vl_map_na)
 
-var vl_map_aunz = "../vega-lite/map_aunz.vl.json"
+var vl_map_aunz = "vega-lite/map_aunz.vl.json"
 vegaEmbed("#map_aunz", vl_map_aunz)
 
-var vl_bar_terrain = "../vega-lite/terrain_type.vl.json"
+var vl_bar_terrain = "vega-lite/terrain_type.vl.json"
 vegaEmbed("#bar_terrain", vl_bar_terrain)
 
 // var vg_1 = "../vega-lite/map.vl.json"; vegaEmbed("#map", vg_1).then(function(result) {
