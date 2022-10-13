b7d169205ce856d939a357efe32fd7e5bb971256
Aiden Guerin <aiden.guerin@outlook.com>
2022-10-13 12:16:51 +1100

    modified geo file

---
+++ web-page/vegalite_vis.js
@@ -0,0 +1,8 @@
+console.log("vegalite_vis.js loaded");
+
+var vl_map = "../vega-lite/map.vl.json"
+vegaEmbed("#map", vl_map)
+
+// var vg_1 = "../vega-lite/map.vl.json"; vegaEmbed("#map", vg_1).then(function(result) {
+//     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+//     }).catch(console.error);
