d80ba05ccdb341ce52719c39c3581be8aee521e6
Aiden Guerin <aiden.guerin@outlook.com>
2022-10-16 16:55:17 +1100

    few edits lol

--- embed_vis.js
+++ embed_vis.js
@@ -6,8 +6,11 @@ vegaEmbed("#map_na", vl_map_na)
 var vl_map_aunz = "vega-lite/map_aunz.vl.json"
 vegaEmbed("#map_aunz", vl_map_aunz)
 
-var vl_bar_terrain = "vega-lite/terrain_type.vl.json"
-vegaEmbed("#bar_terrain", vl_bar_terrain)
+var vl_bar_lifts = "vega-lite/total_lifts.vl.json"
+vegaEmbed("#bar_lifts", vl_bar_lifts)
+
+var vl_bar_runs = "vega-lite/terrain_difficulty.vl.json"
+vegaEmbed("#bar_runs", vl_bar_runs)
 
 // var vg_1 = "../vega-lite/map.vl.json"; vegaEmbed("#map", vg_1).then(function(result) {
 //     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
